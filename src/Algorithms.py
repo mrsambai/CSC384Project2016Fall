0922b8087565d575ac9ff361dad49f93947957a0
Sam <Sam@inside-65-98-56.wireless.utoronto.ca>
2016-12-04 15:09:09 -0500

    starting with basic random strategy

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -2,3 +2,10 @@
 
 algorithms.py should contain the search algirthm and strategies we'll implment to achieve 2 AI playing Othello.
 '''
+
+import random
+
+def strategy_random(board, player):
+    
+    return
+
